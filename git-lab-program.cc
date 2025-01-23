3bb96365b5df4008c0201f9c7ec24a3978b9eeb7
Gabriel <gh343123@ohio.edu>
2025-01-22 20:17:10 -0500

    Code Done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gabriel Hemrick 
+ *        Date: January 22 2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
